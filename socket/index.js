91edccce6066a7ec7fb9cac7c41083ee8bbf2049
LaraibAzmat <azmatlaraib2003@gmail.com>
2024-07-29 19:55:14 +0500

    socket connect

--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,7 @@ const app = express()
 const server = http.createServer(app)
 const io= new Server(server,{
     cors:{
-        origin: window.location.origin,
+        origin: process.env.CORS_ORIGIN,
         credentials: true
     }
 })
